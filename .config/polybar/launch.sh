299eefe4842599b2ed61bfa1f1d043e0d204aec8
y421 <yaeljuego@live.com.mx>
2022-08-01 21:18:49 -0500

    autorandr added

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,9 +2,13 @@
 
 # Terminate already running bar instances
 killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+# Launch bar
+polybar iDisplay &
 
-echo "Bars launched..."
+my_laptop_external_monitor=$(xrandr --query | grep 'DP-1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar eDisplay &
+fi
